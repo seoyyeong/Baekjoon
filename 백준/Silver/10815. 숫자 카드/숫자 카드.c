261c4ed235e27f2bfef94aebf54cf934f7c9443a
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2023-01-10 21:25:47 +0900

    [Silver V] Title: 숫자 카드, Time: 356 ms, Memory: 4980 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const int* a, const int* b) {
+	return *(int*)a > *(int*)b;
+}
+
+int search(int arr[], int n, int s) {
+	int mid, left = 0, right = n - 1;
+	while (right >= left) {
+		mid = (left + right) / 2;
+		if (s == arr[mid]) return 1;
+		if (s < arr[mid]) right = mid - 1;
+		else left = mid + 1;
+	}
+	return 0;
+}
+
+int main(void) {
+	int n, m, s;
+
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	qsort(arr, n, sizeof(int), cmp);
+
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &s);
+		printf("%d ", search(arr, n, s));
+	}
+
+	free(arr);
+
+	return 0;
+}
