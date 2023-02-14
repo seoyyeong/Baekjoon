b930fd0d7c04256c8ab4899ac9baea51d9805533
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2023-02-14 21:12:43 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 64 ms, Memory: 1504 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int* arr = (int*)malloc(sizeof(int) * n);
+
+    scanf("%d", &arr[0]);
+
+    for (int i = 1; i < n; i++) {
+        int t;
+        scanf("%d", &t);
+        arr[i] = t + arr[i - 1];
+    }
+
+    for (int j = 0; j < m; j++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        a--;
+        b--;
+        if (a == 0) printf("%d\n", arr[b]);
+        else printf("%d\n", arr[b] - arr[a - 1]);
+    }
+
+    free(arr);
+    return 0;
+}
