fb1fd2d7091c1d79a8b0568a9663f8a02100dde0
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2023-02-23 22:27:55 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+
+	c += d;
+	b += c / 60;
+	c %= 60;
+	a += b / 60;
+	b %= 60;
+	a %= 24;
+
+	printf("%d %d %d", a, b, c);
+	
+	return 0;
+}
