943784da7639a2d191ed445b21991b6a6c2cd9f2
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2023-03-02 23:25:29 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int cmp(const int a, const int b) {
+	if (a >= b) return a;
+	else return b;
+}
+
+int main(void) {
+	int n, cnt = 1;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	int* dp = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+		dp[i] = 1;
+	}
+	
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > arr[i])
+				dp[i] = cmp(dp[i], dp[j] + 1);
+		}
+		cnt = cmp(cnt, dp[i]);
+	}
+
+	printf("%d", cnt);
+	return 0;
+}
