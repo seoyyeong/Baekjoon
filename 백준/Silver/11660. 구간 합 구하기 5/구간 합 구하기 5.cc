c49dc5a34ca8701d82f0239a31e101b552baa57e
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2024-08-14 23:12:25 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 104 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,31 +1,39 @@
 #include <iostream>
+#define dfARR_MAX 1025
 
-int arr[1025][1025];
+using namespace std;
+
+int arr[dfARR_MAX][dfARR_MAX];
 
 int main(void)
 {
-	int n;
-	int m;
-	int num;
-	int x1;
-	int y1;
-	int x2;
-	int y2;
-
-	scanf("%d %d", &n, &m);
-
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			scanf("%d", &num);
-			arr[i][j] = arr[i][j - 1] + arr[i - 1][j] - arr[i - 1][j - 1] + num;
-		}
-	}
-
-	for (int i = 0; i < m; i++)
-	{
-		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-		printf("%d\n", arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1]);
-	}
+    int n;
+    int m;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin>>n>>m;
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cin>>arr[i][j];
+            arr[i][j]+=(arr[i-1][j]+arr[i][j-1]-arr[i-1][j-1]);
+        }
+    }
+        
+    for(int i=0;i<m;i++)
+    {
+        cin>>x1>>y1>>x2>>y2;
+        cout<<arr[x2][y2]-arr[x2][y1-1]-arr[x1-1][y2]+arr[x1-1][y1-1]<<'\n';
+    }
+    
+    return 0;
 }
