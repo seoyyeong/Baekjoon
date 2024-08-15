78786a40367186943cdaeda814fb66935c227ac9
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2024-08-16 04:36:54 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 368 ms, Memory: 27252 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -1,84 +1,101 @@
-
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-vector<vector<int>> graph;
+vector<vector<int>> v;
 queue<int> q;
 vector<int> ans;
-vector<int> visited;
-int n;
-int m;
-int k;
-int x;
-int cnt;
+bool checked[300'001];
+vector<int> depth;
 
-void BFS(void);
+void BFS(int num);
 
 int main(void)
 {
-	int temp1;
-	int temp2;
+    int n;
+    int m;
+    int k;
+    int x;
 
-	scanf("%d %d %d %d", &n, &m, &k, &x);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	if (k == 0)
-	{
-		printf("%d", x);
-		return 0;
-	}
+    cin >> n >> m >> k >> x;
 
-	graph.resize(n + 1);
-	visited.resize(n + 1, -1);
+    for (int i = 0; i <= n; i++)
+    {
+        vector<int> temp;
+        v.push_back(temp);
+    }
+    depth.resize(n + 1);
 
-	for (int i = 0; i < m; i++)
-	{
-		scanf("%d %d", &temp1, &temp2);
-		graph[temp1].push_back(temp2);
-	}
+    for (int i = 0; i < m; i++)
+    {
+        int a;
+        int b;
+        cin >> a >> b;
+        v[a].push_back(b);
+    }
 
-	BFS();
+    BFS(x);
 
-	for (int i = 1; i <= n; i++)
-	{
-		if (visited[i] == k)
-			ans.push_back(i);
-	}
+    depth[x] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (depth[i] == k)
+        {
+            ans.push_back(i);
+        }
+    }
+    sort(ans.begin(), ans.end());
 
-	if (ans.size() == 0)
-	{
-		printf("-1");
-		return 0;
-	}
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << '\n';
+    }
 
-	vector<int>::iterator iter;
-	for (iter = ans.begin(); iter != ans.end(); ++iter)
-	{
-		printf("%d\n", *iter);
-	}
+    if (ans.size() == 0)
+    {
+        cout << -1 << '\n';
+    }
 
-	return 0;
+    return 0;
 }
 
-void BFS(void)
+void BFS(int num)
 {
-	q.push(x);
-	visited[x]++;
+    int node;
+    int f;
 
-	while (q.size() > 0)
-	{
-		int cur = q.front();
-		q.pop();
-		for (int i = 0; i < graph[cur].size(); i++)
-		{
-			if (visited[graph[cur][i]] == -1)
-			{
-				visited[graph[cur][i]] = visited[cur] + 1;
-				q.push(graph[cur][i]);
-			}
-		}
-	}
+    depth[num] = 0;
+    q.push(num);
+
+    while (!q.empty())
+    {
+        node = q.front();
+        q.pop();
+
+        for (int i = 0; i < v[node].size(); i++)
+        {
+            f = v[node][i];
+            if (checked[f] == true)
+            {
+                if (depth[f] > depth[node] + 1)
+                {
+                    depth[f] = depth[node] + 1;
+                }
+            }
+            else
+            {
+                checked[f] = true;
+                depth[f] = depth[node] + 1;
+                q.push(f);
+            }
+        }
+    }
 
 }
