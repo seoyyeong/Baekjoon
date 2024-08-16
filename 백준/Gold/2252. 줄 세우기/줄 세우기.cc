a211c430dc8774aa6a490d71e0a7a144501b84c2
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2024-08-16 17:59:41 +0900

    [Gold III] Title: 줄 세우기, Time: 24 ms, Memory: 3968 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -5,51 +5,71 @@
 using namespace std;
 
 vector<vector<int>> v;
-vector<int> order;
+vector<int> degree;
 queue<int> q;
 int n;
 int m;
 
+void tsort(void);
 
 int main(void)
 {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin>>n>>m;
+    
+    degree.resize(n+1);
+    for(int i=0;i<=n;i++)
+    {
+        vector<int> temp;
+        v.push_back(temp);
+    }
+    
+    for(int i=0;i<m;i++)
+    {
+        int a;
+        int b;
+        
+        cin>>a>>b;
+        v[a].push_back(b);
+        degree[b]++;
+    }
+    
+    tsort();
+    
+    return 0;
+}
 
-	scanf("%d %d", &n, &m);
-	v.resize(n + 1);
-	order.resize(n + 1);
-
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		scanf("%d %d", &a, &b);
-		v[a].push_back(b);
-		order[b]++; 
-	}
-
-	for (int i = 1; i <= n; i++)
-	{
-		if (order[i] == 0)
-		{
-			q.push(i);
-		}
-	}
-
-	while (q.size() > 0)
-	{
-		int  i = q.front();
-		q.pop();
-		printf("%d ", i);
-
-		for (int j = 0; j < v[i].size(); j++)
-		{
-			order[v[i][j]]--;
-
-			if (order[v[i][j]] == 0)
-			{
-				q.push(v[i][j]);
-			}
-		}
-	}
-
-	return 0;
+void tsort(void)
+{
+    for(int i=1;i<=n;i++)
+    {
+        if(degree[i]==0)
+        {
+            q.push(i);
+        }
+    }
+    
+    while(!q.empty())
+    {
+        int temp;
+        temp = q.front();
+        q.pop();
+        
+        cout<<temp<<' ';
+        
+        for(int i=0;i<v[temp].size();i++)
+        {
+            degree[v[temp][i]]--;
+            if(degree[v[temp][i]]==0)
+            {
+                q.push(v[temp][i]);
+            }
+        }
+    }
+    
+    
 }
